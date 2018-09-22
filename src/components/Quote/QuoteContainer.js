6d055d6b5f1cd3134812ff2786eb442c1ba4abd7
bbenefield89 <bsquared18@gmail.com>
2018-09-21 22:06:28 -0400

    IMPORT <ButtonContainer />
    - in src/components/Quote/QuoteContainer.js

--- src/components/Quote/QuoteContainer.js
+++ src/components/Quote/QuoteContainer.js
@@ -3,14 +3,17 @@ import axios from "axios";
 import { Container, Row, Col } from "reactstrap";
 import styled from "styled-components";
 
-import Anchor from "../misc/Anchor";
-import Button from "../misc/Button";
+import ButtonsContainer from '../ButtonsContainer/ButtonsContainer'
 import QuoteText from "./modules/QuoteText";
 
 const StyledContainer = styled(Container)`
   background: #fdfdfd;
   border-radius: 3px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  margin-left: 1rem !important;
+  margin-right: 1rem !important;
   padding: 20px;
+  max-width: 600px !important;
 `;
 
 class QuoteContainer extends Component {
@@ -18,8 +21,8 @@ class QuoteContainer extends Component {
     super(props);
     this.state = {
       quoteContent: {
-        quote: "",
-        author: ""
+        quote: '',
+        author: ''
       },
       tweetURL: '',
       baseURL: 'https://twitter.com/intent/tweet?hashtags=quotes&text='
@@ -68,17 +71,8 @@ class QuoteContainer extends Component {
 
         <Row>
           <Col>
-            <Anchor
-              targetATTR="_blank"
-              title="Tweet this quote!"
-              tweetURL={this.state.tweetURL}
-              value="Tweet This"
-            />
-
-            <Button
-              title="Generate a new quote"
-              value="New quote"
-              getRandomQuote={this.getRandomQuote}
+            <ButtonsContainer
+              // TODO: SEND DOWN VALUES FOR baseURL, tweetURL, and quoteContent
             />
           </Col>
         </Row>

dde3d799c7acb6a0bff0b74ea97ac079fa977d33
bbenefield89 <bsquared18@gmail.com>
2018-09-22 15:54:29 -0400

    MOVE quoteReducer INTO ITS OWN FILE
    - in src/reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,7 @@
 import { combineReducers } from 'redux'
 
-import {
-  GET_QUOTE_CONTENT
-} from '../actions'
-
-const initialState = {
-  gettingQuotes: true,
-  quoteData: {},
-  tweetURL: ''
-}
-
-export const getQuoteContent = (state=initialState, action) => {
-  switch (action.type) {
-    case GET_QUOTE_CONTENT:
-      const { author, quote, tweetURL } = action.payload
-
-      state = {
-        tweetURL,
-        gettingQuotes: false,
-        quoteData: { author, quote }
-      }
-
-      return state
-
-    default:
-      return state
-  }
-}
+import quoteReducer from './quoteReducer'
 
 export const reducers = combineReducers({
-  quoteContent: getQuoteContent
+  quoteContent: quoteReducer
 })

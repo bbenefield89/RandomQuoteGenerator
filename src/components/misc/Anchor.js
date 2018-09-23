db982f5602c0ccfad8202eb0bc418c1ecd851d3f
bbenefield89 <bsquared18@gmail.com>
2018-09-23 10:42:24 -0400

    STYLES
    - in src/components/Quote/QuoteContainer.js
    - in src/components/misc/Anchor.js
    - in src/components/misc/Button.js

--- src/components/misc/Anchor.js
+++ src/components/misc/Anchor.js
@@ -3,10 +3,28 @@ import { string } from 'prop-types'
 import styled from "styled-components";
 
 const StyledAnchor = styled.a`
-  background: #50B1A7
+  background: hsla(174, 38%, 50%, 1);
+  border: 1px solid hsl(173, 32%, 36%);
   border-radius: 3px;
+  box-shadow: 0 1px 0px hsl(173, 32%, 36%);
   color: #fdfdfd;
   padding: 5px;
+  text-decoration: none !important
+  text-shadow: 0 1px 0 hsl(173, 32%, 36%);
+  transition: background 0.25s
+  
+  &:hover {
+    background: #50b1a7e6;
+    color: #fdfdfd;
+    cursor: pointer;
+    text-shadow: 0 1px 0 hsl(173, 32%, 36%);
+  }
+
+  &:active {
+    background: hsla(174, 33%, 50%, 1);
+    box-shadow: inset 0 1px 0 hsl(173, 32%, 36%);
+    text-shadow: none;
+  }
 `;
 
 const Anchor = props => {

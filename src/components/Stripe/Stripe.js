a78d032f98784a22303b339051aadfb96175c94d
bbenefield89 <bsquared18@gmail.com>
2018-09-23 19:40:00 -0400

    SEND SUCCESSFUL TEST REQ TO Stripe
    - in src/components/Stripe/Stripe.js

--- src/components/Stripe/Stripe.js
+++ src/components/Stripe/Stripe.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import axios from 'axios'
 import StripeCheckout from 'react-stripe-checkout'
+import styled from 'styled-components'
+
+const StyledP = styled.p`
+  color: #fefefe;
+`
+
+const StyledSub = styled.small`
+  color: #fefefe;
+`
 
 const Stripe = () => {
+  const onToken = token => {
+    const req = {
+      method: 'POST',
+      url: 'http://localhost:3001/donations',
+      data: token
+    }
+    
+    axios(req)
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
+  
   return (
-    <StripeCheckout />
+    <React.Fragment>
+      <StyledP>Consider Donating only $1.34USD to me for no reason at all</StyledP>
+
+      <StripeCheckout
+        email='bsquared18@gmail.com'
+        token={ onToken }
+        stripeKey='pk_test_sm2QijqfOE0vqBK0c7W0CYGV'
+      />
+    </React.Fragment>
   )
 }
  
